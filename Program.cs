7c5b473b77f1334ecf7b3efef8d4ff98acbf530d
Носков Георгий Станиславович <gosha.noskov.03@mail.ru>
2026-02-25 18:09:05 +0500

    Добавлено взаимодействие с пользователем

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CalculatorApp
+﻿static void Main(string[] args)
 {
-    internal class Program
+    Console.WriteLine("Калькулятор. Доступные операции: +, -, *, /");
+    while (true)
     {
-        static void Main(string[] args)
+        Console.Write("Введите операцию (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("\nВведите первое число: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        double b = double.Parse(Console.ReadLine());
+        double result = 0;
+        switch (op)
         {
-            Console.WriteLine("Hello, World!");
+            case "+": result = Calculator.Sum(a, b); break;
+            case "-": result = Calculator.Substract(a, b); break;
+            case "*": result = Calculator.Multiply(a, b); break;
+            case "/":
+                if (b != 0) result = Calculator.Divide(a, b);
+                else Console.WriteLine("Ошибка: деление на ноль!");
+                break;
+            default:
+                Console.WriteLine("Неизвестная операция!");
+                continue;
         }
+        Console.WriteLine($"Результат: {result}");
     }
 }
